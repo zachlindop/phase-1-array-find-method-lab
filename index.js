357dbf0ca6c11a2e2151de45b30a3190f1dc5c64
zachlindop <85177168+zachlindop@users.noreply.github.com>
2021-07-22 20:18:00 -0700

    commit tree d868a0e1b6f32743f655140857ca932bf7469ccb

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function superbowlWin(record){
+  const foundSeason = record.find(season => season.result ==="W")
+  return foundSeason ? foundSeason.year : undefined
+}
 
+
+
+
+
+
+
+
+
+
+//function superbowlWin(myArray) {
+//    let yearWin = myArray.find(element => element.result === "W")
+//      return yearWin.year
+//  }
+//  superbowlWin(record)
+//
+//
+//function isOdd(element, index, array) {
+//    return (element % 2 === 1);
+//  }
+// 
+//  
+//  function superbowlWin(bigArray){
+//      let yearWin = bigArray.find(element)
+//}
